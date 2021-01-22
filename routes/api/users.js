91ea605532d8c8c06b6d55bb27c1c4e1cb2757f0
VanVDC <vanbvy@gmail.com>
2021-01-22 18:21:57 -0500

    install validator and rm express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 import express from 'express';
 const router = express.Router();
+import { check, validationResult } from 'express-validator/check';
 
-//@route GET api/users
+//@route POST api/users
 //@desc test route
 //@acess Public
-router.get('/', (req, res) => res.send('User route'));
+router
+  .route('/')
+  .get(
+    [
+      check('name', 'Name is required').not().isEmpty(),
+      check('email', 'Please include a valid email ').isEmail(),
+      check(
+        'password',
+        'Please enter a password with 6 or more characters '
+      ).isLength({ min: 6 }),
+    ],
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      res.send('User route');
+    }
+  );
 
 export default router;
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import connectDB from './config/db.js';
+import users from './routes/api/users.js';
 
 const app = express();
 
 //connect database
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 //define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', users);
+// app.use('/api/auth', auth);
+// app.use('/api/profile', profile);
+// app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
