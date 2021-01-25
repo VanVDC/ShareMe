87cc2ee86f8fd9adc004e052cbbba1eea6cb9b39
VanVDC <vanbvy@gmail.com>
2021-01-25 15:46:45 -0500

    added protect middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,15 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, addUser } from '../controllers/userController.js';
+import {
+  authUser,
+  addUser,
+  deleteUser,
+} from '../controllers/userController.js';
+
+import { protect } from '../middleware/authMiddleware.js';
+
 router.route('/').post(addUser);
 router.post('/login', authUser);
+router.route('/:id').delete(protect, deleteUser);
 
 export default router;
