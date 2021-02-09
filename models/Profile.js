2b084c1e75a9b9a2c7baa706700b8ca6e2f27306
VanVDC <vanbvy@gmail.com>
2021-02-09 17:14:41 -0500

    added Profile model

---
+++ models/Profile.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+});
