f5b9ca894f19bf5c8c146793b86590ab0a6f77c8
VanVDC <vanbvy@gmail.com>
2021-02-11 17:55:59 -0500

    added get all posts route and controllers

--- controllers/postController.js
+++ controllers/postController.js
@@ -16,14 +16,28 @@ export const createPost = asyncHandler(async (req, res) => {
   }
   try {
     const user = await User.findById(req.user.id).select('-password');
-    const newPost = {
+    const newPost = new Post({
       text: req.body.text,
       name: user.name,
       avatar: user.avatar,
       user: req.user.id,
-    };
+    });
     const post = await newPost.save();
     res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+//@route GET /api/posts
+//@desc Get all posts
+//@access Private
+
+export const getAllPosts = asyncHandler(async (req, res) => {
+  try {
+    const posts = await Post.find().soft({ date: -1 });
+
+    res.json(posts);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { createPost } from '../controllers/postController.js';
+import { createPost, getAllPosts } from '../controllers/postController.js';
 const router = express.Router();
 
-router.route('/').post(protect, createPost);
+router.route('/').post(protect, createPost).get(protect, getAllPosts);
 
 export default router;
