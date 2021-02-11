f724fd7b48326e90fa426550f71a1a9286892277
VanVDC <vanbvy@gmail.com>
2021-02-11 18:50:35 -0500

    fixed route Post error

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -15,5 +15,5 @@ router.route('/').post(protect, createPost).get(protect, getAllPosts);
 router.route('/:id').get(protect, getPostById).delete(protect, deletePost);
 router.route('/like/:id').put(protect, likeAPost);
 router.route('/unlike/:id').put(protect, unlikeAPost);
-router.route('/comment/:id').POST(protect, commentOnAPost);
+router.route('/comment/:id').post(protect, commentOnAPost);
 export default router;
