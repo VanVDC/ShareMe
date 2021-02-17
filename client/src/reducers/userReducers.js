9492589993c707a2fec9c0a1d4b62d6459cbbe86
VanVDC <vanbvy@gmail.com>
2021-02-16 20:02:31 -0500

    added user login and add user reducers

---
+++ client/src/reducers/userReducers.js
@@ -0,0 +1,52 @@
+import {
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+  USER_ADD_REQUEST,
+  USER_ADD_SUCCESS,
+  USER_ADD_FAIL,
+} from '../constants/userConstants';
+
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null,
+};
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return {
+        loading: true,
+      };
+    case USER_LOGIN_SUCCESS:
+      return {
+        loading: false,
+        userInfo: action.payload,
+      };
+    case USER_LOGIN_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userAddReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_ADD_REQUEST:
+      return { loading: true };
+    case USER_ADD_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_ADD_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
