b995b17100d5f458c40c28872c05f01e6a164bee
VanVDC <vanbvy@gmail.com>
2021-02-16 19:23:49 -0500

    added login and addUser actions

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import {
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_FAIL,
+  USER_ADD_REQUEST,
+  USER_ADD_SUCCESS,
+} from '../constants/userConstants';
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    );
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (err) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const addUser = ({ name, email, password }) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    dispatch({ type: USER_ADD_REQUEST });
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    );
+    dispatch({
+      type: USER_ADD_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
