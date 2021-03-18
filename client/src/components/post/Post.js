5dc80ffa251bdcefab6fabd42b6dbb5f6b2067c6
VanVDC <vanbvy@gmail.com>
2021-03-18 19:53:00 -0400

    added commentItem with delete comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,12 +3,15 @@ import { useDispatch, useSelector } from 'react-redux';
 // import spinner
 import PostItem from '../posts/PostItem';
 import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/postActions';
 import { Link } from 'react-router-dom';
+
 const Post = ({ match }) => {
   const dispatch = useDispatch();
   const postFromState = useSelector((state) => state.post);
   const { post, loading } = postFromState;
+
   useEffect(() => {
     dispatch(getPost(match.params.id));
   }, [getPost]);
@@ -21,6 +24,11 @@ const Post = ({ match }) => {
       </Link>
       <PostItem post={post} showAction={false} />
       <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => {
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />;
+        })}
+      </div>
     </Fragment>
   );
 };
